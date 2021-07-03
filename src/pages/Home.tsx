83dfee301c3c071cfdb04cea3af7dda6ce2936da
Gabriel Pereira <gabriel9938@gmail.com>
2021-07-03 11:59:58 -0300

    feat: Finalizando desafio

---
+++ src/pages/Home.tsx
@@ -0,0 +1,59 @@
+import React, { useState } from 'react';
+
+import { Header } from '../components/Header';
+import { MyTasksList } from '../components/MyTasksList';
+import { TodoInput } from '../components/TodoInput';
+
+interface Task {
+  id: number;
+  title: string;
+  done: boolean;
+}
+
+export function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+
+  function handleAddTask(newTaskTitle: string) {
+    if(newTaskTitle){
+      setTasks(oldTasks => [...oldTasks, {
+        id: new Date().getTime(),
+        title: newTaskTitle,
+        done: false,
+      }])
+    }
+  }
+
+  function handleMarkTaskAsDone(id: number) {
+    const updatedTasks = tasks.map( task => {
+      if(task.id === id) {
+        return {
+          ...task,
+          done: true
+        }
+      }
+
+      return {...task}
+    })
+
+    setTasks(updatedTasks)
+  }
+
+  function handleRemoveTask(id: number) {
+    const filteredTasks = tasks.filter( task => task.id !== id)
+    setTasks(filteredTasks)
+  }
+
+  return (
+    <>
+      <Header />
+
+      <TodoInput addTask={handleAddTask} />
+
+      <MyTasksList 
+        tasks={tasks} 
+        onPress={handleMarkTaskAsDone} 
+        onLongPress={handleRemoveTask} 
+      />
+    </>
+  )
+}
